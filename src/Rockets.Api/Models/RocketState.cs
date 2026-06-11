12629c24daeebc7e9f87a08bb3af048c20fec624
ziMMi99 <simon@mvbmail.dk>
2026-06-11 21:00:24 +0200

    Add RocketState.cs

---
+++ src/Rockets.Api/Models/RocketState.cs
@@ -0,0 +1,13 @@
+﻿namespace Rockets.Api.Models;
+
+public class RocketState {
+	
+	public Guid Id { get; init; }
+	public bool IsLaunched { get; set; }
+	public bool IsTerminated { get; set; }
+	public string? Mission { get; set; }
+	public string? RocketType { get; set; }
+	public double Speed { get; set; }
+	public int LastMessageNumber { get; set; }
+	public string? ExplosionReason { get; set; }
+}
