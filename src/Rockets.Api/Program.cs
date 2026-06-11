6d57304948c20a61396b41fb08d90e3c22375457
ziMMi99 <simon@mvbmail.dk>
2026-06-11 21:04:45 +0200

    Add endpoints to send messages, get a rocket by id, get all rockets and a health-check for smoke testing

--- src/Rockets.Api/Program.cs
+++ src/Rockets.Api/Program.cs
@@ -1,7 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+using Rockets.Api.Models;
+using Rockets.Api.Services;
 
-var app = builder.Build();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IMessageHandler, MessageHandler>();
+builder.Services.AddScoped<IMessageParser, MessageParser>();
+builder.Services.AddSingleton<IRocketStateService, RocketStateService>();
+
+
+WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.MapPost("/messages", (MessageEnvelope envelope, IMessageHandler handler) => {
+	handler.Handle(envelope);
+	return Results.Ok();
+});
+
+app.MapGet("/rocket/{id:Guid}", (Guid id, IRocketStateService rocketStateService) => {
+	RocketState? state = rocketStateService.Get(id);
+	return state is null ? Results.NotFound() : Results.Ok(state);
+});
+
+app.MapGet("/rockets", (IRocketStateService rocketStateService) => Results.Ok((object?)rocketStateService.GetAll()));
+
+app.MapGet("/health-check", async () => Results.Ok());
 
 app.Run();
