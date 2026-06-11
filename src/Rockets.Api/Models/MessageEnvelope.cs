94d793db04b60856c87d2e9baf8ba7e7bbdeac24
ziMMi99 <simon@mvbmail.dk>
2026-06-11 21:00:38 +0200

    Add MessageEnvelope.cs

---
+++ src/Rockets.Api/Models/MessageEnvelope.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace Rockets.Api.Models;
+
+public class MessageEnvelope {
+	public MessageMetadata Metadata { get; set; }
+	public JsonElement Message { get; set; }
+}
+
+public class MessageMetadata  {
+	public Guid Channel { get; set; }
+	public int MessageNumber { get; set; }
+	public DateTimeOffset MessageTime { get; set; }
+	public string MessageType { get; set; } = string.Empty;
+}
